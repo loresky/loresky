c661e3e46569900898711bfb5a87d462ccb1f064
cy <cy>
2015-04-24 15:49:44 +0800

    加了注释

--- app/src/main/java/com/loresky/zoom/test/glide/GlideActivity.java
+++ app/src/main/java/com/loresky/zoom/test/glide/GlideActivity.java
@@ -2,20 +2,19 @@ package com.loresky.zoom.test.glide;
 
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.gc.materialdesign.views.ButtonRectangle;
 import com.loresky.zoom.R;
 import com.loresky.zoom.common.BaseActivity;
-import com.loresky.zoom.util.DebugLog;
 
 /**
  * Created by cy on 15-4-23.
  */
 public class GlideActivity extends BaseActivity implements OnClickListener {
-    private Button requestBtn;
+    private ButtonRectangle requestBtn;
     private ImageView mImage;
 
     @Override
@@ -25,7 +24,7 @@ public class GlideActivity extends BaseActivity implements OnClickListener {
 
     @Override
     public void initViews() {
-        requestBtn = (Button) findViewById(R.id.request);
+        requestBtn = (ButtonRectangle) findViewById(R.id.request);
         mImage = (ImageView) findViewById(R.id.image);
     }
 
@@ -52,7 +51,8 @@ public class GlideActivity extends BaseActivity implements OnClickListener {
     private void loadImage() {
         String url = "http://d.hiphotos.baidu.com/image/pic/item/08f790529822720ea751464078cb0a46f31fabe9.jpg";
         Glide.with(this).load(url)
-                .override(300,200)
+//                .override(300,200)
+//                .centerCrop()
                 .placeholder(R.mipmap.ic_launcher)
                 .error(R.drawable.abc_ab_share_pack_mtrl_alpha)
                 .diskCacheStrategy(DiskCacheStrategy.ALL).into(mImage);
